c72f42109c94a71301654e917c0d0a7bf2c6fdd2
omnya zerghouni <omnyazer@gmail.com>
2025-12-19 14:57:48 +0100

    flavicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-
-    {{-- Police Poppins --}}
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css')
 </head>
-<body style="background-color: #f5f8fa; font-family: 'Poppins', sans-serif;">
+
+<body>
 
     @include('partials.header')
 
