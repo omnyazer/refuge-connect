6b7fbe5f07ec8c995fc6651e6740d1d94db9d799
omnya zerghouni <omnyazer@gmail.com>
2025-12-19 14:13:28 +0100

    Style header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header class="rc-header">
+    <div class="rc-header-inner">
+        <div class="rc-header-logo">
+            <img src="{{ asset('images/logo (1).png') }}" alt="RefugeConnect" class="rc-logo">
+        </div>
+        <nav class="rc-nav">
+            <a href="{{ route('home') }}">Accueil</a>
+            <a href="{{ route('animals.create') }}">Ajouter un animal</a>
+        </nav>
+    </div>
+</header>
